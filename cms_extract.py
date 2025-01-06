320367f9bce247136dc5783164fa44637afa5628
KwesiLarbi <KwesiiiLarb@gmail.com>
2025-01-06 14:36:27 -0500

    extracted data from cms and saved as csv file

--- cms_extract.py
+++ cms_extract.py
@@ -1,6 +1,10 @@
-def data_pipeline(url):
-    return
+from utils import process_monthly_enrollment_data
 
+def data_pipeline(url):
+    process_monthly_enrollment_data(url)
+
+    return 'Success'
+ 
 if __name__ == '__main__':
-    url = ''
+    url = 'https://data.cms.gov/data-api/v1/dataset/d7fabe1e-d19b-4333-9eff-e80e0643f2fd/data'
     data_pipeline(url)
--- utils.py
+++ utils.py
@@ -1,19 +1,57 @@
 import requests
-import zipfile
 import io
 import pandas as pd
+from datetime import datetime
 
-def download_zip(url):
-    """Download a csv file from a URL"""
+def process_monthly_enrollment_data(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()
+        status = response.raise_for_status()
+
+        if response.status_code == 200 and status == None:
+            # parse the response content as JSON
+            data = response.json()
+
+            # extract enrollement features
+            features = []
+            for key in data[0].keys():
+                features.append(key)
+            
+            print('current features...\n')
+
+            for feature in features:
+                print(feature)
+
+            # # create a list of dictionaries containing data
+            monthly_enrollments = []
+
+            todays_date = datetime.now()
+
+            date = todays_date.strftime('%Y_%m_%d')
+
+            # file destination
+            file = f'C:\\Users\\kwesi\\data_projects\\cms_data_pipeline\\data\\{date}_monthly_enrollments.csv'
+
+            for i in data:
+                monthly_enrollment = {
+                    'year': i['YEAR'],
+                    'month': i['MONTH'],
+                    'state': i['BENE_STATE_ABRVTN'],
+                    'total_beneficiaries': i['TOT_BENES'],
+                    'male_total_beneficiaries': i['MALE_TOT_BENES'],
+                    'female_total_beneficiaries': i['FEMALE_TOT_BENES'],
+                    'black_total_beneficiaries': i['BLACK_TOT_BENES'],
+                    'white_total_beneficiaries': i['WHITE_TOT_BENES'],
+                    'hspnc_total_beneficiaries': i['HSPNC_TOT_BENES']
+                }
+                monthly_enrollments.append(monthly_enrollment)
+            
+            # convert the list of dictionaries to a DataFrame
+            df = pd.DataFrame(monthly_enrollments)
+
+            # save to csv in specific location
+            df.to_csv(file, index=False)
+
         return io.BytesIO(response.content)
     except requests.exceptions.RequestException as e:
-        raise Exception(f'Failed to download the file from {url}. Error: {e}')
-
-def unzip_files(zip_content, output_folder='./data'):
-    with zipfile.ZipFile(zip_content, 'r') as zip_file:
-        # Extract all files to the output folder
-        zip_file.extractall(output_folder)
-        print(f"Files extracted to {output_folder}")
+        raise Exception(f'Failed to retrieve data from {url}. Error: {e}')
